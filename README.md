8c2e712b1a250ed2de510a6fdb4b5a809b6f6004
丁双拥 <18560459059@sina.com>
2019-06-01 18:47:02 +0800

    添加队列控制api 代码重构

--- README.md
+++ README.md
@@ -9,40 +9,53 @@ npm i --save @zyyz/vue-async-data
 ```java script
 import AsyncDataMixin from '@zyyz/vue-async-data'
 {
-name: 'app',
-mixins: [AsyncDataMixin],
-asyncData: {
-  message: {// data name
-    watch:['id'],// watch a data or prop to reload data
-    init () {//a function to init the data
-      return ''
+  name: 'app',
+  mixins: [AsyncDataMixin],
+  asyncData: {
+    message: {// data name
+      watch:['id'],// 监听某些状态，触发刷新
+      init () {// 初始化data
+        return ''
+      },
+      fetch () {// 异步拉取数据，需要返回一个Promise
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            resolve('hello world')
+          }, 1000)
+        })
+      },
+      error (response) {
+        
+      }
     },
-    fetch () {// a function to fetch data from remote,must return a promise object
-      return new Promise((resolve) => {
-        setTimeout(() => {
-          resolve('hello world')
-        }, 1000)
-      })
-    },
-    error (response) {
-      //do some thing with response
+    otherSomeData: {
+      init () {
+        return 1
+      },
+      fetch () {
+        return new Promise((resolve, reject) => {
+          ..........
+        })
+      }
+    }
+  
+  },
+  data () {
+    return {
+      id: 0
     }
   },
-  otherSomeData: {
-    init () {
-      return 1
+  methods: {
+    refresh () {// 强制刷新
+      this.$async.fetch('message')
+      this.$async.fetch('otherSomeData')
     },
-    fetch () {
-      return new Promise((resolve, reject) => {
-        ..........
-      })
+    suspend () {
+      this.$async.suspend()
+    },
+    resume () {
+      this.$async.resume()
     }
-  }
-  // all promise object will be resolved by Promise.all()
-},
-data () {
-  return {
-    id: 0
   }
 }
 ```
@@ -52,9 +65,19 @@ data () {
 init和fetch这两个选项是必填的，fetch函数返回的必须是一个Promise对象。watch选项可以watch一个或多个data、prop或computed，其实就是利用了$watch()。
 请求数据的时机是activated和mounted这两个生命周期钩子。fetch和error中的this是当前的vm对象，和method一样。
 asyncData和data一样支持mixin。
-
+如果想强制刷新数据，可以调用this.$fetch('dataName')
+## details
+延时刷新： 
+异步数据的拉取和vue的render机制差不多，其内部存在一个队列。一个异步数据需要被拉取时，它会加入到队列中。队列会在nextTick中被执行，这样就实现了一个delay机制，防止在一个tick中重复刷新。 
+队列控制： 
+为了精准控制是否刷新，本插件提供了api，以便对队列进行控制。 
+this.&#36;async.suspend() 可以将队列挂起，队列不会在nextTick中自动执行，即暂停了自动刷新机制
+this.&#36;async.resume() 结束挂起状态，恢复自动刷新机制，如果当前有挂起的队列，则立即执行队列
+this.&#36;async.clean() 可以清空队列
+强制刷新: 
+this.&#36;async.fetch('dataName') 强制将一个异步数据加入到队列当中
 ## rollup
-已经用rollup对src目录下的代码进行了打包，并用babel转译了一下。但是Promise等polyfill没有做处理，polyfill都是在项目中处理的。
+已经用rollup对src目录下的代码进行了打包，并用babel转译了一下。但是Promise等polyfill没有做处理。
 
 ## 源码构建
 ```bash
--- src/index.js
+++ src/index.js
@@ -1,67 +1,126 @@
 import {isDef, isFunction} from './util.js'
 import Vue from 'vue'
-function addToQuene (vm, varName) {
-  let quene = vm._asyncDataCtrl.quene
-  if (quene.indexOf(varName) === -1) {
-    quene.push(varName)
+
+// 队列控制器
+class AsyncController {
+  constructor (vm, options) {
+    this.vm = vm
+    this.options = options
+    this._asyncDataCtrl = {
+      hasWaitQue: false,
+      suspend: false,
+      queue: []
+    }
+  }
+  init () {
+    let vm = this.vm
+    let options = this.options
+    let varNames = Object.keys(options)
+    varNames.forEach((varName) => {
+      let option = options[varName]
+      // 初始化数据
+      if (isFunction(option.init)) {
+        this.initVar(varName, option.init)
+      }
+      // 添加watch
+      if (isDef(option.watch)) {
+        option.watch.forEach((v) => {
+          vm.$watch(v, () => {
+            this.fetchVar(varName)
+          })
+        })
+      }
+    })
   }
 
-  if (!vm._asyncDataCtrl.hasWaitQue) {
-    //在下一个tick开始发起队列内的异步请求
-    vm.$nextTick(() => {
-      let queneCpy = quene.slice(0)
-      quene.length = 0
-      vm._asyncDataCtrl.hasWaitQue = false
-      queneCpy.forEach((varName) => {
-        let option = vm.$options.asyncData[varName]
-        let promise =  option.fetch.call(vm)
-        return promise.then((resp) => {
-          // merge to data when success
-          vm[varName] = resp
-        }, (resp) => {
-          // call error hook or log
-          if (isFunction(option.error)) {
-            option.error.call(vm, resp)
-          } else {
-            window.console.warn(`you got a error when fecth asyncData[${varName}] from remote`)
-          }
-        })
+  initVar (name, initFunc) {
+    let vm = this.vm
+    if (!isDef(vm[name])) {
+      vm.$set(vm, name, initFunc())
+    } else {
+      vm[name] = initFunc()
+    }
+  }
+
+  addToQueue (varName) {
+    let vm = this.vm
+    let queue = this._asyncDataCtrl.queue
+
+    if (queue.indexOf(varName) === -1) {
+      queue.push(varName)
+    }
+    if (this._asyncDataCtrl.suspend) {
+      return
+    }
+    // 在nextTick刷新数据
+    if (!this._asyncDataCtrl.hasWaitQue) {
+      //添加一个延时任务 在下一个tick开始发起队列内的异步请求
+      vm.$nextTick(() => {
+        this._asyncDataCtrl.hasWaitQue = false
+        if (!this._asyncDataCtrl.suspend) {
+          this.executeQueue()
+        }
+      })
+      this._asyncDataCtrl.hasWaitQue = true
+    }
+  }
+  executeQueue () {
+    let vm = this.vm
+    let options = this.options
+    let queue = this._asyncDataCtrl.queue
+    if (queue.length == 0) {
+      return
+    }
+    // copy一下队列， 屏蔽在后面的执行过程中触发addToQueue(如果可能的话)引起的队列增长
+    let queueCpy = queue.slice(0)
+    queue.length = 0
+    queueCpy.forEach((varName) => {
+      let option = options[varName]
+      let promise =  option.fetch.call(vm)
+      let errorHandler = option.error
+      promise.then((resp) => {
+        // merge to data when success
+        vm[varName] = resp
+      }, (resp) => {
+        // call error hook or log
+        if (isFunction(errorHandler)) {
+          errorHandler.call(vm, resp)
+        } else {
+          window.console.warn(`you got a error when fecth asyncData[${varName}] from remote`)
+        }
       })
     })
-    vm._asyncDataCtrl.hasWaitQue = true
   }
-}
-
-function fetchVar (vm, varName) {
-  addToQuene(vm, varName)
-}
-
-function initVar (vm, name, initFunc) {
-  if (!isDef(vm[name])) {
-    vm.$set(vm, name, initFunc())
-  } else {
-    vm[name] = initFunc()
+  fetchVar (varName) {
+    this.addToQueue(varName)
   }
-}
 
-function fetchData (vm, options) {
-  Object.keys(options)
-  .forEach((varName) => {
-    fetchVar(vm, varName)
-  })
-}
-
-function validityOptions (options) {
-  let result = true
-  Object.keys(options).forEach((key) => {
-    let option = options[key]
-    if (!isDef(option) || !isFunction(option.fetch) || !isFunction(option.init)) {
-      result = false
+  fetchData () {
+    let options = this.options
+    Object.keys(options)
+    .forEach((varName) => {
+      this.fetchVar(varName)
+    })
+  }
+  fetch (varName) {
+    this.fetchVar(varName)
+  }
+  suspend () {
+    this._asyncDataCtrl.suspend = true
+  }
+  resume () {
+    this._asyncDataCtrl.suspend = false
+    // 如果有处于等待的任务，则交给它执行刷新
+    if (this._asyncDataCtrl.hasWaitQue) {
+      return
     }
-  })
-  return result
+    this.executeQueue()
+  }
+  clean () {
+    this._asyncDataCtrl.queue.length = 0;
+  }
 }
-// 因为$set的限制，所以要预先添加根数据
+// 因为$set的限制，所以要改造$option.data 预先添加根数据
 function preSetAsyncData (vm, options) {
   let varNames = Object.keys(options)
   let _dataOption = vm.$options.data
@@ -73,39 +132,28 @@ function preSetAsyncData (vm, options) {
     return Object.assign(_dataOption(), _preSetAsyncData)
   }
 }
-// 初始化数据，添加watch
-function initAsyncData (vm, options) {
+// 验证option
+function validityOptions (options) {
+  let result = true
+  Object.keys(options).forEach((key) => {
+    let option = options[key]
+    if (!isDef(option) || !isFunction(option.fetch) || !isFunction(option.init)) {
+      result = false
+    }
+  })
+  return result
+}
+// 构造
+function createAsyncData (vm, options) {
   if (!validityOptions(options)) {
     throw new Error('async_data.err.options_volidate_fail')
   }
-  // 添加控制器
-  vm._asyncDataCtrl = {
-    hasWaitQue: false,
-    quene: []
-  }
-  // 添加$fetch，用来强制刷新数据
-  vm.$fetch = function (varName) {
-    fetchVar(this, varName)
-  }
-  
-  let varNames = Object.keys(options)
-  varNames.forEach((varName) => {
-    let option = options[varName]
-    // 初始化数据
-    if (isFunction(option.init)) {
-      initVar(vm, varName, option.init)
-    }
-    // 添加watch
-    if (isDef(option.watch)) {
-      option.watch.forEach((v) => {
-        vm.$watch(v, () => {
-          fetchVar(vm, varName)
-        })
-      })
-    }
-  })
+  // 添加队列控制
+  var async = new AsyncController(vm, options)
+  return async
 }
 
+// mixin
 var AsyncDataMixin = {
   beforeCreate () {
     let options = this.$options.asyncData
@@ -116,20 +164,19 @@ var AsyncDataMixin = {
   created () {
     let options = this.$options.asyncData
     if (isDef(options) && Object.keys(options).length > 0) {
-      initAsyncData(this, options)
+      this.$async = createAsyncData(this, options)
+      this.$async.init()
     }
   },
   
   activated () {
-    if (isDef(this._asyncDataCtrl)) {
-      let options = this.$options.asyncData
-      fetchData(this, options)
+    if (isDef(this.$async)) {
+      this.$async.fetchData()
     }
   },
   mounted () {
-    if (isDef(this._asyncDataCtrl)) {
-      let options = this.$options.asyncData
-      fetchData(this, options)
+    if (isDef(this.$async)) {
+      this.$async.fetchData()
     }
   }
 }
